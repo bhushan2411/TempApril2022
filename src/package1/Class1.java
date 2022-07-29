ad6ef9319ed1c715dee09da8a5b5c6c105edf966
Bhushan Gadakh <bhushan.gadakh94@gmail.com>
2022-07-29 08:15:19 +0530

    BhushanBranch_A

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,5 +3,9 @@ package package1;
 public class Class1 {
 public static void main(String[] args) {
 	System.out.println(123);
+	System.out.println(1233);
+	System.out.println("BHUSHAN Project upload branch A");
+	System.out.println(" Project upload branch A");
+	System.out.println("Project upload branch A");
 }
 }
