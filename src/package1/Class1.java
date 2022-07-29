46c64d71bb8e1ee2a113bf7e2fbabe8130564f47
Bhushan Gadakh <bhushan.gadakh94@gmail.com>
2022-07-29 19:01:55 +0530

    EBranch

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -2,6 +2,8 @@ package package1;
 
 public class Class1 {
 public static void main(String[] args) {
-	System.out.println(123);
+	System.out.println("Branch E");
+	System.out.println("Branch E1");
+	System.out.println("Branch E2");
 }
 }
