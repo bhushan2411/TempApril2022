d74d847cb0e27cf954e92bad45504fac92da8a66
Bhushan Gadakh <bhushan.gadakh94@gmail.com>
2022-07-29 19:29:23 +0530

    BHUSHan

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,17 +3,14 @@ package package1;
 public class Class1 {
 public static void main(String[] args) {
 	System.out.println(123);
-<<<<<<< Updated upstream
+
 	System.out.println(1233);
 	System.out.println("BHUSHAN Project upload branch A");
 	System.out.println(" Project upload branch A");
 	System.out.println("Project upload branch A");
 	System.out.println("Barnch B");
 	System.out.println("Barnch B1");
-
-=======
-	System.out.println("Barnch B");
+    System.out.println("Barnch B");
 	System.out.println("Barnch B1");
->>>>>>> Stashed changes
 }
 }
