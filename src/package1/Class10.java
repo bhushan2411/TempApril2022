62a531e7539f81b9c4f69320e3c82411633ae201
Bhushan Gadakh <bhushan.gadakh94@gmail.com>
2022-07-29 18:49:54 +0530

    CLASS10BranchC

---
+++ src/package1/Class10.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Class10 {
+public static void main(String[] args) {
+	System.out.println("Branch C");
+	System.out.println("Branch C1");
+	System.out.println("Branch C2");
+}
+}
